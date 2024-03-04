9609e6dccb577758749ecb257b982dc8f928dd64
Justin Mauldin <justin.mauldin7@gmail.com>
2024-03-04 14:41:51 -0700

    Adds the "validates_presence_of" for all the fields on the table for the Booking model to get the tests to pass.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,10 @@
 class Booking < ApplicationRecord
-
+  validates_presence_of :first_name,
+                        :last_name,
+                        :animal_name,
+                        :animal_type,
+                        :hours_requested,
+                        :date_of_service
 end
 
 
