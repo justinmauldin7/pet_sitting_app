1423071d36617d4e9dda457a9b191dbed33daeb5
Justin Mauldin <justin.mauldin7@gmail.com>
2024-03-05 10:25:22 -0700

    Adds the sad path test to the Bookings create booking feature test if not all fields are filled in.

--- spec/features/user_can_create_new_booking_spec.rb
+++ spec/features/user_can_create_new_booking_spec.rb
@@ -36,4 +36,25 @@ describe "user creates new booking on booking new page" do
     expect(current_path).to eq(new_booking_path)
     expect(page).to have_content("Booking record successfully created.")
   end
+
+  it "DOES NOT create a new booking" do
+    expect {
+              visit new_booking_path
+
+              # skipping filing in the 'booking_first_name' field so the creation will fail.
+              fill_in(:booking_last_name, with: valid_booking_params[:last_name])
+              fill_in(:booking_animal_name, with: valid_booking_params[:animal_name])
+
+              select(valid_booking_params[:animal_type], from: 'booking_animal_type')
+              select(valid_booking_params[:hours_requested], from: 'booking_hours_requested')
+
+              select(valid_booking_params[:date_of_service][:year], from: 'booking_date_of_service_1i')
+              select(valid_booking_params[:date_of_service][:month], from: 'booking_date_of_service_2i')
+              select(valid_booking_params[:date_of_service][:day], from: 'booking_date_of_service_3i')
+
+              click_on "Submit Booking"
+            }.to change { Booking.count }.by(0)
+
+    expect(page).to have_content("ERROR: Booking NOT created.")
+  end
 end
