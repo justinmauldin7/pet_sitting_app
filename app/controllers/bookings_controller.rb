46ac0c4af6e21ae18cbc005ea25543c49be86909
Justin Mauldin <justin.mauldin7@gmail.com>
2024-03-05 08:39:55 -0700

    Adds the "booking_params" method to the BookingsController so that we can permit the creation of the Booking record.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,17 @@ class BookingsController < ApplicationController
   end
 
   def create 
- 
+
+  end
+
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:first_name,
+                                    :last_name,
+                                    :animal_name,
+                                    :animal_type,
+                                    :hours_requested,
+                                    :date_of_service)
   end
 end
